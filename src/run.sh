baef4609163f542dc08f43aef02ce8da0581a2b5
Armin Biere <biere@jku.at>
2020-07-04 11:16:46 +0200

    running

--- src/run.sh
+++
@@ -1,7 +0,0 @@
-##!/bin/sh
-kitten=`kitten $1|grep '^s'`
-kissat=`kissat $1|grep '^s'`
-echo "kitten $kitten"
-echo "kissat $kissat"
-[ "$kitten" = "$kissat" ] && exit 20
-exit 1
--- test/.gdbinit
+++
@@ -1,5 +0,0 @@
-tty /dev/pts/2
-file tissat
-b execute_command
-b tissat_error
-run
